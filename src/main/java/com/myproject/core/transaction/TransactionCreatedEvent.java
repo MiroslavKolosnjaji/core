6bdbe6551c78bd9a98920e80730168d0723f7115
Miroslav <kolosnjaji@stcable.net>
2024-12-30 11:57:36 +0100

    Created 'TransactionCreatedEvent' class.

---
+++ src/main/java/com/myproject/core/transaction/TransactionCreatedEvent.java
@@ -0,0 +1,28 @@
+package com.myproject.core.transaction;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.Instant;
+
+/**
+ * @author Miroslav Kološnjaji
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class TransactionCreatedEvent {
+
+    private Long transactionId;
+    private Long userId;
+    private String accountNumber;
+    private BigDecimal amount;
+    private BigDecimal balance;
+    private String transactionType;
+    private String status;
+    private Instant createdAt;
+}
